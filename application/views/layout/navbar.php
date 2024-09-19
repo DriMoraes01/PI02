e9a00a3dee4bf645167cce22c47d114c25da4f96
Adriele de Moraes <146091307+DriMoraes01@users.noreply.github.com>
2024-09-19 13:16:28 -0300

    update navbar.php

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -1,4 +1,4 @@
-<header class="header-top" header-theme="orange">
+<header class="header-top" header-theme="red">
     <div class="container-fluid">
         <div class="d-flex justify-content-between">
             <div class="top-menu d-flex align-items-center">
